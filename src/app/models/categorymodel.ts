6837149aa4cd3032c7488e3f2c5b6a31faaff6cd
santimarce <marchulo1996@gmail.com>
2023-06-13 09:47:53 -0500

    Cambio en servicios

---
+++ src/app/models/categorymodel.ts
@@ -0,0 +1,5 @@
+interface CategoryModel{
+    id:number;
+    name:string;
+    description:string;
+}
--- src/app/models/productmodel.ts
+++ src/app/models/productmodel.ts
@@ -1,9 +1,8 @@
-interface productModel{
+interface ProductModel{
     id:number;
     tittle:string;
     description:string;
     price:number;
     category:string;
     images:string[];
-    
 }
